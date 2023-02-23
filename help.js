690e42920c6fa6ae1ec7bbd9c37d0f6d5c527b33
EsaStore21 <125125106+EsaStore21@users.noreply.github.com>
2023-02-23 09:22:31 +0700

    Add files via upload

---
+++ help.js
@@ -0,0 +1,19 @@
+
+exports.infoOwner = () =>{
+return`──「 *DATA PROFIL OWNER* 」──
+
+ ⬣ *Nama :* Rama 
+ ⬣ *Umur :* Privv
+ ⬣ *Hoby. :* Turu/Game
+ ⬣ *Asal   :* Jatim - Lamongan
+ ⬣ *Status:* Creator
+ ⬣ *Zodiak:* Gk tau
+
+                  「 *SOSIAL MEDIA*」
+ ⬣ *Whatsapp:* 085791220179
+ ⬣ *Youtube:* Ramaa Gnnz
+ ⬣ *Github:* ---
+ ⬣ *Tiktok:* ---
+ ⬣ *Instagram:* ---
+ `
+}
